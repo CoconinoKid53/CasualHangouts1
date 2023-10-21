c86c42132b8e71d54fb017a62ddd034dd9156120
Coconinokid53 <bigdg2014@hotmail.com>
2023-10-21 00:22:19 -0600

    Needs To be Fixed

---
+++ app/src/main/java/com/example/casualhangouts/CoolHangouts#4.kt
@@ -0,0 +1,51 @@
+package com.example.casualhangouts
+
+class CoolHangouts {
+
+
+    fun main() {
+        var myAddress = 236
+        fun myFunction(int: Int) = print(myAddress)
+
+        myFunction(myAddress)
+        println(myAddress)
+
+        val myStreet = "West st."
+        fun myFunction (str: String) = print(myStreet)
+
+        val myEvent = "BBQ"
+
+
+        val myState = "Idaho"
+
+        val myTime = "7:00pm"
+
+
+
+        var greeting: String = "Welcome to CasualHangouts"
+
+        val congratulations = arrayOf("236, West st., BBQ, Idaho, 7:00pm")
+        for (c in congratulations) {
+            print(c)
+        }
+    }
+}
+fun rule(args: Array<Boolean>) {
+    val myRules = ("no cursing, no drinking, no immorality")
+    fun myFunction(str: String) = print(myRules)
+    if (False) {
+        (" Ban Account")
+    } else if(True) {
+
+        ("Access Granted")
+    }
+}
+
+
+fun friend () {
+    var addfriend = "New Friend"
+    fun myFunction(str: String) = print(addfriend)
+
+    myfunction(addfriend)
+    println(addfriend)
+}
